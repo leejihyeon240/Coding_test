6b40f4244016e18b6b4f4686aa3a144c76bddd94
leejihyeon240 <xnaxna1@naver.com>
2024-11-04 17:51:11 +0900

    2024.11.04 코딩테스트

---
+++ programmers/src/training/day1/OutputString.java
@@ -0,0 +1,14 @@
+package training.day1;
+
+import java.util.Scanner;
+
+public class OutputString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        System.out.println(a);
+	}
+
+}
