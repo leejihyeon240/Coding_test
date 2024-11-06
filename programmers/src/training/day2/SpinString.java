ad1d25fc780d2f3edfab2a32488a5f0c035cf962
leejihyeon240 <xnaxna1@naver.com>
2024-11-06 17:47:41 +0900

    2024.11.06 코딩테스트

---
+++ programmers/src/training/day2/SpinString.java
@@ -0,0 +1,17 @@
+package training.day2;
+
+import java.util.Scanner;
+
+public class SpinString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        /* 문자열 담아서 for문으로 하나씩 출력? */
+        for(int i = 0 ; i < a.length();i++) {
+        	// 원하는 위치의 문자 가져오기
+        	System.out.println(a.charAt(i));
+	}
+
+}
